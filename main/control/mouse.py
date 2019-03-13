0c430c20b2e5ba7a91aa15cc7fcce0b92b935aed
huangwenzi <35164459+huangwenzi@users.noreply.github.com>
2019-03-13 11:15:44 +0800

    huang
    鼠标对点击事件的返回

--- main/control/mouse.py
+++ main/control/mouse.py
@@ -3,12 +3,14 @@
 # 三方模块
 import pygame
 # 项目模块
+from enums.game_enum import game_enum
 
 # 鼠标事件的返回
 class Postion():
-    def __init__(self, tmp_type):
-        self.type = tmp_type
-        self.position = [0, 0]
+    def __init__(self):
+        self.type = game_enum.mouse.click_free
+        self.x = 0
+        self.y = 0
 
 # 鼠标事件处理
 class Mouse():
@@ -20,14 +22,15 @@ class Mouse():
     # 鼠标事件
     # type : 0:无操作, 1:点击左键, 2:左键弹起
     def mouse_event(self):
-        pressde = pygame.mouse.get_pressed()  # 获取鼠标按下信息
-        position = pygame.mouse.get_pos()  # 获取鼠标位置
-        ret = Postion(0)
-        ret.position = position
+        pressde = pygame.mouse.get_pressed()    # 获取鼠标按下信息
+        position = pygame.mouse.get_pos()       # 获取鼠标位置
+        ret = Postion()
+        ret.x = position[0]
+        ret.y = position[1]
         if pressde[0]:
             self.left_set = True
-            ret.type = 1
+            ret.type = game_enum.mouse.click_down
         elif pressde[0] == 0 and self.left_set == True:
             self.left_set = False
-            ret.type = 2
+            ret.type = game_enum.mouse.click_open
         return ret
